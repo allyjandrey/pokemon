8f9ba6a60ef63ad6760530b919d5bc3616da1f8d
Allyson <jandreyallyson@gmail.com>
2023-05-19 19:50:07 -0300

    feat - página Home

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,14 @@
+import styles from './index.module.css'
+
+export const Home = () => {
+    return (
+        <section className={styles.section_container}>
+            <main>
+                <h1><strong>Find </strong>
+                    all your <br /> favorite <br />
+                    <strong> Pokemon</strong>
+                </h1>
+            </main>
+        </section>
+    )
+}
