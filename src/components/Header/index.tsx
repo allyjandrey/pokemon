2af920c1eb4a3ac511dc80af98cdc23506636596
Allyson <jandreyallyson@gmail.com>
2023-05-19 19:08:50 -0300

    feat - header da página

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import styles from './index.module.css'
+import logo from '../../assets/Logo.svg'
+
 export const Header = () => {
     return (
-        <header>
-            
+        <header className={styles.header}>
+            <img className={styles.img} src={logo} alt="Logo do pokémon" />
+                <a>Home</a>
+                <a>Pokedéx</a>
+                <a>Legendaries</a>
+                <a>Documentation</a>
         </header>
     )
 }
