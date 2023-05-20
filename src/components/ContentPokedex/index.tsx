e332783387cd7f63f3c1a352f901546531a9c72b
Allyson <jandreyallyson@gmail.com>
2023-05-20 20:59:54 -0300

    feat - tela pokedex

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -1,5 +1,14 @@
+import styles from './index.module.css'
+
 export const ContentPokedex = () => {
     return (
-        <h1>Pokedex</h1>
+        <section>
+            <article>
+                <h1>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
+            </article>
+            <article>
+            <input className={styles.input_container} type="text" placeholder="Find your Pokemon"/>
+            </article>
+        </section>
     )
 }
