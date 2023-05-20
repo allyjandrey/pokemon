52fefa78a8bab157244a6a1e0396ef9b7fb6a1f2
Allyson <jandreyallyson@gmail.com>
2023-05-20 20:32:51 -0300

    feat - criação da rota para a página Pokedex

---
+++ src/routes/Pokedex.tsx
@@ -0,0 +1,11 @@
+import { ContentPokedex } from "../components/ContentPokedex"
+import { Header } from "../components/Header"
+
+export const Pokedex = () => {
+    return (
+        <>
+        <Header />
+        <ContentPokedex />
+        </>
+    )
+}
