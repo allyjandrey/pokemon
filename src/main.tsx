d92f246a76f6f9933a38fe29d48cd8e86ea9b54f
Allyson <jandreyallyson@gmail.com>
2023-05-20 20:32:03 -0300

    feat - adicionando rota da página Pokedex

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './global.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Home } from './routes/Home.tsx';
+import { ContentPokedex } from './components/ContentPokedex/index.tsx';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/Pokedex",
+        element: <ContentPokedex />,
       },
     ],
   },
