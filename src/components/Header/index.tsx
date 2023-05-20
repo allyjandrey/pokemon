900efa68d704b410bfa8ade8c007f6cc1544594b
Allyson <jandreyallyson@gmail.com>
2023-05-20 14:36:04 -0300

    hotfix - alinhamento das palavras no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,15 @@ import logo from '../../assets/Logo.svg'
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <img className={styles.img} src={logo} alt="Logo do pokémon" />
-                <a>Home</a>
-                <a>Pokedéx</a>
-                <a>Legendaries</a>
-                <a>Documentation</a>
+            <div className={styles.div_container}>
+                <img className={styles.img} src={logo} alt="Logo do pokémon" />
+                <nav>
+                    <a>Home</a>
+                    <a>Pokedéx</a>
+                    <a>Legendaries</a>
+                    <a>Documentation</a>
+                </nav>
+            </div>
         </header>
     )
 }
