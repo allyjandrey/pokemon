8a01699b1ee6b87597e28df06e58cfa9cf5a3de8
Allyson <jandreyallyson@gmail.com>
2023-05-20 16:32:22 -0300

    feat - adição das rotas

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HomeRoute } from './routes/homeRoute.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeRoute />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
