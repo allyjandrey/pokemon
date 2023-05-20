6b91e547fb76691b8fb16686207db474ab64ee16
Allyson <jandreyallyson@gmail.com>
2023-05-19 23:16:13 -0300

    hotfix - ajuste do conteúdo em tela da Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,18 +7,20 @@ export const Home = () => {
         <section className={styles.section_container}>
             <main>
                 <article className={styles.article_container}>
-                    <h1><strong>Find </strong>
-                        all your <br /> favorite <br />
-                        <strong> Pokemon</strong>
-                    </h1>
-                    <h2>You can know the type of Pokemon, <br />
-                        it's strenghts, disadvantages and <br />
-                        abilities
-                    </h2>
-                    <button className={styles.button}>See pokemons</button>
-                </article>
-                <article>
-                    <img src={banner} alt="Banner do Pikachu com duas pokébolas próximas a ele e nuvens atrás" />
+                    <article>
+                        <h1><strong>Find </strong>
+                            all your <br /> favorite <br />
+                            <strong> Pokemon</strong>
+                        </h1>
+                        <h2>You can know the type of Pokemon, <br />
+                            it's strenghts, disadvantages and <br />
+                            abilities
+                        </h2>
+                        <button className={styles.button}>See pokemons</button>
+                    </article>
+                    <article>
+                        <img src={banner} alt="Banner do Pikachu com duas pokébolas próximas a ele e nuvens atrás" />
+                    </article>
                 </article>
             </main>
         </section>
