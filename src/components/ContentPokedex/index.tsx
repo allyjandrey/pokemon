d8b03a5ac86ed534d1d96012247da90ab73dd508
Allyson <jandreyallyson@gmail.com>
2023-05-21 17:04:21 -0300

    refactor - arrumando elementos em tela

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -4,13 +4,13 @@ export const ContentPokedex = () => {
     return (
         <section className={styles.section_container}>
             <main>
-                <article>
+                <article className={styles.article_container}>
                     <h1 className={styles.h1_class}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
                 </article>
-                <article>
+                <article className={styles.article_container_input}>
                     <input className={styles.input_container} type="text" placeholder="Find your Pokemon" />
                 </article>
-                <article>
+                <article className={styles.article_container_select}>
                     <select>
                         <option disabled selected hidden>Type</option>
                     </select>
