c50a1469d69e4e03e377061d275c9725f425e4e6
Allyson <jandreyallyson@gmail.com>
2023-05-21 16:09:53 -0300

    feat - adicionando selects e options

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -3,12 +3,25 @@ import styles from './index.module.css'
 export const ContentPokedex = () => {
     return (
         <section className={styles.section_container}>
-            <article>
-                <h1 className={styles.h1_class}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
-            </article>
-            <article>
-            <input className={styles.input_container} type="text" placeholder="Find your Pokemon"/>
-            </article>
+            <main>
+                <article>
+                    <h1 className={styles.h1_class}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
+                </article>
+                <article>
+                    <input className={styles.input_container} type="text" placeholder="Find your Pokemon" />
+                </article>
+                <article>
+                    <select>
+                        <option>Type</option>
+                    </select>
+                    <select>
+                        <option>Attack</option>
+                    </select>
+                    <select>
+                        <option>Experience</option>
+                    </select>
+                </article>
+            </main>
         </section>
     )
 }
