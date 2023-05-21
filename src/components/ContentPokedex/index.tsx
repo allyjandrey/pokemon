93b74ecd5a70c463faf9b050bd4ce7ba67665156
Allyson <jandreyallyson@gmail.com>
2023-05-20 21:10:19 -0300

    refactor - arrumando tela

--- src/components/ContentPokedex/index.tsx
+++ src/components/ContentPokedex/index.tsx
@@ -2,9 +2,9 @@ import styles from './index.module.css'
 
 export const ContentPokedex = () => {
     return (
-        <section>
+        <section className={styles.section_container}>
             <article>
-                <h1>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
+                <h1 className={styles.h1_class}>800 <strong>Pokemons</strong> for you to choose your favorite</h1>
             </article>
             <article>
             <input className={styles.input_container} type="text" placeholder="Find your Pokemon"/>
