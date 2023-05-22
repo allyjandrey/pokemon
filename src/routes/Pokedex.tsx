f727e1f5b39bb6a2d51c2ff2e828d1b38c013286
Allyson <jandreyallyson@gmail.com>
2023-05-22 15:16:41 -0300

    hotfix - remoção da chamada do card na rota pokedex

--- src/routes/Pokedex.tsx
+++ src/routes/Pokedex.tsx
@@ -1,4 +1,3 @@
-import { CardPokemon } from "../components/CardPokemon"
 import { ContentPokedex } from "../components/ContentPokedex"
 import { Header } from "../components/Header"
 
@@ -7,7 +6,6 @@ export const Pokedex = () => {
         <>
         <Header />
         <ContentPokedex />
-        <CardPokemon />
         </>
     )
 }
