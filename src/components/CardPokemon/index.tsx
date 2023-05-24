4b3c110a5c84314b15fcb1ce7c8fbd3df26fbcf0
Allyson <jandreyallyson@gmail.com>
2023-05-24 00:27:45 -0300

    feat - cor de span mudando com o type do pokemon

--- src/components/CardPokemon/index.tsx
+++ src/components/CardPokemon/index.tsx
@@ -42,11 +42,32 @@ export const CardPokemon = () => {
                     }
                 })
             )
-            console.log(dataPokemons);
             setPokemons(dataPokemons);
         }
         getPokemons()
     }, [])
+
+    const PokemonTypeColors = {
+
+        stile: '#A1A1A1',
+        dark: '#A1A1A1',
+        rock: '#A1A1A1',
+        grass: '#70A83B',
+        bug: '#70A83B',
+        ice: '#A2CFF0',
+        water: '#A2CFF0',
+        fire: '#F76745',
+        fighting: '#F76745',
+        dragon: '#F76745',
+        normal: '#76AADB',
+        gosth: '#76AADB',
+        poison: '#A974BC',
+        psychic: '#A974BC',
+        fairy: '#A974BC',
+        ghost: '#A974BC',
+        ground: '#9B597B',
+        electric: '#F7C545'
+    }
 
     return (
         <article className={styles.article_container}>
@@ -65,11 +86,17 @@ export const CardPokemon = () => {
                             <h6>Defense</h6>
                         </article>
                         <article className={styles.card_pokemon}>
-                            <span className={styles.pokemon_element}>{poke.types[0].type.name}</span>
-                            <span className={styles.pokemon_element}>{poke.types.length == 2 ? poke.types[1].type.name : poke.types[0].type.name}</span>
+                            <span
+                                style={{ backgroundColor: PokemonTypeColors[poke.types[0].type.name] }}
+                                className={styles.pokemon_element}>{poke.types[0].type.name}</span>
+                            <span style={{ backgroundColor: poke.types.length == 2 ? PokemonTypeColors[poke.types[1].type.name] : PokemonTypeColors[poke.types[0].type.name] }} className={styles.pokemon_element}>
+                                {poke.types.length == 2 ? poke.types[1].type.name : poke.types[0].type.name}
+                            </span>
                         </article>
                     </article>
-                    <article className={styles.card_container_right}>
+                    <article
+                        style={{ backgroundColor: PokemonTypeColors[poke.types[0].type.name] }}
+                        className={styles.card_container_right}>
                         <img src={poke.image} alt="Imagem do card" className={styles.pokemon_container} />
                     </article>
                 </article>
